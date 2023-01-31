441756a9a9900250a23a8bdca31477da3c1ecb4f
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-31 20:13:01 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import MainEl from './components/Main';
 
 function App() {
   return (
+
     <div className="App">
       {/* Adiconar os componentes que estaram na pasta aqui! */}
       <HeaderEl/>
       <MainEl/>
     </div>
+    
   );
 }
 
