03299dc5e1330ba0b503822f3caca76e971c8ee6
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-31 19:49:10 +0200

    Created the Main Contentetnt

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import HeaderEl from './components/HeaderEl';
+import MainEl from './components/Main';
 
 function App() {
   return (
     <div className="App">
       {/* Adiconar os componentes que estaram na pasta aqui! */}
       <HeaderEl/>
+      <MainEl/>
     </div>
   );
 }
