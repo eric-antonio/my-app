5337049f10a6809fd60d7e8c8c66bc04aa7023ff
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-31 20:10:59 +0200

    Project 01 Done✅

--- src/components/HeaderEl.js
+++ src/components/HeaderEl.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "../index.css"
 import"../App.css"
+import reactLogo from"../img/react-logo.png"
 export default function HeaderEl(){
     return(
        <header>
             <nav className="header-nav">
-                <img src="../img/react-logo.svg" alt="React JS" className="logoHeader"/>
+                <img src={reactLogo} alt="React JS" className="logoHeader"/>
                 <h1>ReactFcats</h1>
                 <ul className="nav-items">
                     <li>React Course - Project 1</li>
