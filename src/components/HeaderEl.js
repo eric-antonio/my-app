44151ba36bf9a68a543b7dce14a8f6cdfe544701
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-31 19:28:42 +0200

    Styling Header

--- src/components/HeaderEl.js
+++ src/components/HeaderEl.js
@@ -1,9 +1,17 @@
 import React from "react";
-
+import "../index.css"
+import"../App.css"
 export default function HeaderEl(){
     return(
-        <nav>
-            <h1>Hello!</h1>
-        </nav>
+       <header>
+            <nav className="header-nav">
+                <img src="../img/react-logo.svg" alt="React JS" className="logoHeader"/>
+                <h1>ReactFcats</h1>
+                <ul className="nav-items">
+                    <li>React Course - Project 1</li>
+
+                </ul>
+            </nav>
+       </header>
     )
 }
