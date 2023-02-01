1967c39d043448ff4d97006eaef7c60a1b5f8b8e
Éric Ivan João António <ericantoniodev@gmail.com>
2023-02-01 19:51:46 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/Main'
+import'./components/Navbar'
+import NavBar from './components/Navbar';
+import Main from './components/Main';
 function App() {
   return (
 
     <div className="App">
       {/* Adiconar os componentes que estaram na pasta aqui! */}
-    
+      <NavBar/>
+      <Main/>
     </div>
     
   );
