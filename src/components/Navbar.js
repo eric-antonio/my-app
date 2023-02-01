3f9e6e8b9aa7bb51a3ef9aff1a08c786135a5681
Éric Ivan João António <ericantoniodev@gmail.com>
2023-02-01 19:51:50 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,12 @@
 import React from "react";
+
+export default function NavBar(){
+    return(
+        <div className="header">
+            <nav>
+                <h1>ReactFacts</h1>
+                <h4>React Course -Project_01</h4>
+            </nav>
+        </div>
+    )
+}
