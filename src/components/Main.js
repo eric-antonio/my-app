cfeddf68d3b8b762cfe676d3ae3a840ce8de2867
Éric Ivan João António <ericantoniodev@gmail.com>
2023-02-01 19:51:48 +0200

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,1 +1,13 @@
 import React from "react";
+
+
+export default function Main(){
+    return(
+        <div className="Main">
+            <h2>Fun facts about React</h2>
+            <ul>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
