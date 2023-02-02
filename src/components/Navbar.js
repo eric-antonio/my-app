f2ca5a60c595fa1ee6693a58b6660d79af43e855
Éric Ivan João António <ericantoniodev@gmail.com>
2023-02-02 17:09:38 +0200

    Nav updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,9 @@ import reactlogo from "../img/react-logo.png"
 export default function NavBar(){
     return(
         <nav>
-            <img src={reactlogo}/>
+            <img src={reactlogo}  className="nav--icon" />
+            <h3  className="nav--logoName">React Facts</h3>
+            <h4  className="nav--projectName">React Course - Project 1</h4>
         </nav>
     )
-}
+} 
