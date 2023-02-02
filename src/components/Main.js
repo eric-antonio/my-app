e0c5fc7655c343b43748dfabaddfe36e573b5ba3
Éric Ivan João António <ericantoniodev@gmail.com>
2023-02-02 17:39:05 +0200

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,8 +4,8 @@ import React from "react";
 export default function Main(){
     return(
         <main>
-            <h1>Fun facts about React</h1>
-            <ul>
+            <h1 className="main--title">Fun facts about React</h1>
+            <ul className="main--facts">
                 <li>Was first released in 2023</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 100k stars on GitHub</li>
